b5153f739d8283368a2644e660c3e30b8786e224
Henry Gómez <43563642+henrygomezn@users.noreply.github.com>
2022-08-31 20:41:19 -0400

    update next

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
   env: {
     REACT_APP_RSA_KEY : process.env.REACT_APP_RSA_KEY,
     REACT_APP_API_URL: process.env.REACT_APP_API_URL,
-    NEXTAUTH_URL: 'http://localhost:3000/'
+    NEXTAUTH_URL: 'https://bando-front.herokuapp.com/'
   }
 }
